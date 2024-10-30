560ca1d4726a4b6db062668ce8b1997876fe1c86
bhandd <bjorn1987@gmail.com>
2024-10-30 17:14:08 +0100

    Initial commit

---
+++ src/main/java/org/example/fullstacklab1/FullStackLab1Application.java
@@ -0,0 +1,21 @@
+package org.example.fullstacklab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class FullStackLab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackLab1Application.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
---
+++ src/test/java/org/example/fullstacklab1/FullStackLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fullstacklab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackLab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
